02aeb8c5effa93b4504b93bac0b1043b05dc5461
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-25 23:22:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <p id = "root">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
      </div>
   )
 }
