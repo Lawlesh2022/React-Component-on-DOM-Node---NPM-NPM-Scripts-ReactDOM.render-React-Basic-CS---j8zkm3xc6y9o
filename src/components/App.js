ba49602482e6f6ae086a49e33b19f8518a62dc45
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-05 22:37:02 +0530

    Use same line export

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import '../styles/App.css';
 
-function App() {
+export default function App() {
   return (
     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
   )
 }
-export default App
