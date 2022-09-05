9d780f256fe0d13827801a40f575e590d01acbac
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-05 22:32:14 +0530

    Try regular function

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../styles/App.css';
 
-const App = () => {
+function App() {
   return (
     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
   )
